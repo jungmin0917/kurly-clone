ce3777b4e9a1074f9c34cd1ade6fd8234a438a74
jungmin0917 <jungmin09172@naver.com>
2023-12-11 18:07:07 +0900

    타임리프 기본 레이아웃 작성 중

---
+++ src/main/java/site/kurly/market/controller/MainController.java
@@ -0,0 +1,18 @@
+package site.kurly.market.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class MainController {
+
+    @GetMapping(value = "/")
+    public String main(Model model){
+        return "index";
+    }
+
+
+}
