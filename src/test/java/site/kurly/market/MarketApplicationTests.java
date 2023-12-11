b9dc3f1dda3db665dc6e1d73cb3f96f9be984295
jungmin0917 <jungmin09172@naver.com>
2023-12-11 12:03:35 +0900

    프로젝트 첫 커밋

---
+++ src/test/java/site/kurly/market/MarketApplicationTests.java
@@ -0,0 +1,13 @@
+package site.kurly.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
