fb7bc6dcb575f45ce38aec3310e67badbc4b76ca
jungmin0917 <jungmin09172@naver.com>
2023-12-14 10:16:17 +0900

    타임리프 라이브 리로드 설정

---
+++ src/main/java/site/kurly/market/controller/MemberController.java
@@ -0,0 +1,19 @@
+package site.kurly.market.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import site.kurly.market.service.MemberService;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    // 로그인 폼으로 이동
+    @GetMapping("/login")
+    public String loginForm(){
+        return "loginForm";
+    }
+}
